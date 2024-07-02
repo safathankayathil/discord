f60bc1cc37a8544a294bbc0492571b54bddc6319
safathankayathil <safathangayathil@gmail.com>
2024-07-02 15:45:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,354 @@
+const data = [
+    {
+        Id:1,
+        name: "MID JOURNEY",
+        img: "./assets/images/562762757905186828/e69c9585f8b23477c0626ac4a96f2794.jpg",
+        description: "Leonardo. Ai is a generative AI platform for content creation. Create game assets, artwork, design elements and more!",
+        cate: "science & tech",
+        online: "1,66,400 Online",
+        members: "1,55,9900 Members",
+        icon: "./assets/icons/1046979304547954728/bbe5d6097255431300cfc18dd629b876.jpg",
+    },
+    {
+        Id:2,
+        name: "VIGGLE",
+        img: "./assets/images/679875946597056683/bb4791a77207e327dfa42c301a2d9603.jpg",
+        description: "Viggle. ai,making any character move as you want!",
+        cate: "science & tech",
+        online: "1,64,500 Online",
+        members: "1,45,9900",
+        icon: "./assets/icons/1181076253172842537/131ad9e0f231d38d30fff2d7c80e2ce8.jpg",
+    },
+    {
+        Id:3,
+        name: "Lime Wire",
+        img: "./assets/images/658675654799130624/a1f0807229ba1d1af94c5685cb0b208e.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "science & tech",
+        online: "1,64,900 Online",
+        members: "1,445,9900",
+        icon: "./assets/icons/1181076253172842537/131ad9e0f231d38d30fff2d7c80e2ce8.jpg",
+    },
+    {
+        Id:4,
+        name: "MONOPOLY GO!",
+        img: "./assets/images/586704051148816385/8d9f4e10f9394ee7d8ba90bec94ef2a0.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "science & tech",
+        online: "1,64,900 Online",
+        members: "1,454,9900",
+        icon: "./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+    },
+    {
+        Id:5,
+        name: "Lime Wire",
+        img: "./assets/images/1102970375731691612/f229a622417118ca41bbcddbd3aa12f4.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "science & tech",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/541484311354933258/a_0f0bbb6cab6f28ce7d60e4818c4550e5.jpg",
+    },
+    {
+        Id:6,
+        name: "Leonardo.Ai",
+        img: "./assets/images/541484311354933258/0e0519045645ed1931c408ec42af9000.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Education",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/150074202727251969/d059a3821ea30f12baf1a5cda8a570e3.jpg",
+    },
+    {
+        Id:7,
+        name: "Blox Fruits",
+        img: "./assets/images/522681957373575168/5167dfbb5aa9b38c4696dc080360c826.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Education",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/398627612299362304/a_792c266092d4989bb9a67e0abe3e965f.jpg",
+    },
+    {
+        Id:8,
+        name: "Nova",
+        img: "./assets/images/398627612299362304/4f1fa86af52e9c3af85e63605ec5748d.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Education",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+    },
+    {
+        Id:9,
+        name: "All Star",
+        img: "./assets/images/1046979304547954728/d0ac123bc0db716e100a5a3a24c62fa8.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Education",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+    },
+    {
+        Id:10,
+        name: "Roblox",
+        img: "./assets/images/1102970375731691612/f229a622417118ca41bbcddbd3aa12f4.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Education",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+    },
+    {
+        Id:11,
+        name: "Fusionist",
+        img: "./assets/images/679875946597056683/bb4791a77207e327dfa42c301a2d9603.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Entertainment",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+    },
+    {
+        Id:12,
+        name: "Valorant",
+        img: "./assets/images/658675654799130624/a1f0807229ba1d1af94c5685cb0b208e.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Entertainment",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/541484311354933258/a_0f0bbb6cab6f28ce7d60e4818c4550e5.jpg",
+    },
+    {
+        Id:13,
+        name: "Lofi Girl",
+        img: "./assets/images/586704051148816385/8d9f4e10f9394ee7d8ba90bec94ef2a0.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Entertainment",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/562762757905186828/05d312e0df9dbae9d43273987a53a794.jpg",
+    },
+    {
+        Id:14,
+        name: "King Legacy",
+        img: "./assets/images/562762757905186828/e69c9585f8b23477c0626ac4a96f2794.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Entertainment",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/586704051148816385/a_0373a32bd3c0a40c117a7dbfc541eaf1.jpg",
+    },
+    {
+        Id:15,
+        name: "RELL Games",
+        img: "./assets/images/541484311354933258/0e0519045645ed1931c408ec42af9000.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "Entertainment",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/662267976984297473/39128f6c9fc33f4c95a27d4c601ad7db.jpg",
+    },
+    {
+        Id:16,
+        name: "Era",
+        img: "./assets/images/522681957373575168/5167dfbb5aa9b38c4696dc080360c826.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "music",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/679875946597056683/a_6103d052635b9c6d1b12ae7a018e1f25.jpg",
+    },
+    {
+        Id:17,
+        name: "No Hesi",
+        img: "./assets/images/398627612299362304/4f1fa86af52e9c3af85e63605ec5748d.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "music",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/710745950380884009/eddb33db0199ca4ded014bd9e29fa07d.jpg",
+    },
+    {
+        Id:18,
+        name: "Stumble Guys",
+        img: "./assets/images/1046979304547954728/d0ac123bc0db716e100a5a3a24c62fa8.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "music",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/710745950380884009/eddb33db0199ca4ded014bd9e29fa07d.jpg",
+    },
+    {
+        Id:19,
+        name: "English",
+        img: "./assets/images/710745950380884009/516d0293ef3c0ba1c2cfe361f04ec484.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "music",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "../assets/icons/727649662475173962/6de4e8a714ec16f66820b7d50a0799c1.jpg",
+    },
+    {
+        Id:20,
+        name: "PUBG MOBILE",
+        img: "./assets/images/679875946597056683/bb4791a77207e327dfa42c301a2d9603.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "music",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+    },
+    {
+        Id:21,
+        name: "Galxe",
+        img: "./assets/images/658675654799130624/a1f0807229ba1d1af94c5685cb0b208e.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "gaming",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/964645662866173972/a_833235c7a3b4b3c9f7337fc2b8475058.jpg",
+    },
+    {
+        Id:22,
+        name: "Rainbow",
+        img: "./assets/images/586704051148816385/8d9f4e10f9394ee7d8ba90bec94ef2a0.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "gaming",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/763643320638898186/a_9cb99e46ca489a6cdb9b8e0c473dc8c6.jpg",
+    },
+    {
+        Id:23,
+        name: "Sui",
+        img: "./assets/images/562762757905186828/e69c9585f8b23477c0626ac4a96f2794.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "gaming",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+    },
+    {
+        Id:24,
+        name: "Live Insaan",
+        img: "./assets/images/541484311354933258/0e0519045645ed1931c408ec42af9000.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "gaming",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+    },
+    {
+        Id:25,
+        name: "Sky",
+        img: "./assets/images/522681957373575168/5167dfbb5aa9b38c4696dc080360c826.jpg",
+        description: "Our active text-to-image AI community powers your journey your to generate thr best art,",
+        cate: "gaming",
+        online: "1,64,900 Online",
+        members: "1,454,9900 Members",
+        icon: "./assets/icons/710745950380884009/eddb33db0199ca4ded014bd9e29fa07d.jpg",
+    },
+];
+
+const productsContainer = document.querySelector(".founds");
+const categoriesContainer = document.querySelector(".category");
+const searchInput = document.querySelector(".search");
+const searchButton = document.querySelector(".searchbutton");
+const resultsCount = document.querySelector(".results-count");
+
+let activeCategory = "All";
+
+const displayProducts = (products) => {
+    productsContainer.innerHTML = products
+      .map(
+        (product) => `
+        <div class="found">
+            <img src=${product.img} alt="" />
+            <div class="content">
+                <div class="top">
+                    <span><img src=${product.icon} /></span>
+                    <h3>${product.name}</h3>
+                </div>
+                <p>${product.description}</p>
+                <ul>
+                    <li>${product.online}</li>
+                    <li class="list">${product.members}</li>
+                </ul>
+            </div>
+        </div>`
+      )
+      .join("");
+    resultsCount.textContent = `${products.length} Results Found`;
+  };
+
+const setCategories = () => {
+displayProducts(data);
+
+const allcategory = data.map((item) => item.cate);
+const categories = ["All", ...new Set(allcategory)];
+
+categoriesContainer.innerHTML = categories
+.map(
+  (cate) => `
+  <li class="cate">
+      <a href="#">
+        <i class="fa fa-${getIconClass(cate)}"></i> ${cate}
+      </a>
+      <h6 class="count">${cate === "All" ? data.length : data.filter((item) => item.cate === cate).length}</h6>
+  </li>`
+)
+.join("");
+
+categoriesContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI" && e.target.classList.contains("cate")) {
+    const categoryName = e.target.querySelector("a").textContent.trim();
+    activeCategory = categoryName;
+    filterProducts(searchInput.value);
+
+    const categoriesElements = document.querySelectorAll(".cate");
+    categoriesElements.forEach(category => {
+      category.classList.remove('active');
+    });
+    e.target.classList.add('active');
+  }
+});
+
+const allCategory = document.querySelector('.cate:first-child');
+allCategory.classList.add('active'); 
+};
+
+const getIconClass = (category) => {
+    switch (category.toLowerCase()) {
+      case "gaming":
+        return "gamepad";
+      case "education":
+        return "graduation-cap";
+      case "entertainment":
+        return "tv";
+      case "all":
+        return "compass";
+      case "music":
+        return "music";
+      case "science & tech":
+        return "atom";
+      default:
+        return "compass";
+    }
+  };
+
+  const filterProducts = (searchTerm) => {
+    const filteredProducts = data.filter((item) => {
+      const matchesCategory = activeCategory === "All" || item.cate === activeCategory;
+      const matchesSearchTerm = item.name.toLowerCase().includes(searchTerm.toLowerCase());
+      return matchesCategory && matchesSearchTerm;
+    });
+    displayProducts(filteredProducts);
+  };  
+
+  searchButton.addEventListener("click", () => {
+    filterProducts(searchInput.value);
+  });
+  
+  displayProducts(data);
+  setCategories();
